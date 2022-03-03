29a51f822f40248a609604a6e28a20963df571f5
Jarjanazi <64444222+mrvanonmus@users.noreply.github.com>
2022-03-03 14:49:15 +0200

    Update SEA-fish.rb

--- SEA-fish.rb
+++ SEA-fish.rb
@@ -11,12 +11,12 @@ puts("\n\033[1;37m          {1}Wishsix")
 puts("\033[1;37m            {2}Blank page")
 puts("\033[1;37m            {3}Allow follow-up requests")
 puts("\033[1;37m            {4}amino")
-puts("\033[1;37m            {5}Blank page")
+puts("\033[1;37m            {5}cam")
 
 
 puts("\n\n\033[1;33m[=====exit=====]\n\n")
 
-print("\n       enter the number")
+print("'\n\e[1;92m[\e[0m\e[1;77mSEA\e[0m\e[1;92m] Enter the number : \e[0m")
 num=gets.chomp.to_i
  if num ==1
   system("cd Wishsix")
@@ -29,18 +29,23 @@ num=gets.chomp.to_i
   system("bash 1.sh")
   
  elsif num ==3
-   system("cd Allow")
+  system("cd Allow")
   
-   system("bash 1.sh")
+  system("bash 1.sh")
    
-  elsif num ==4
-   system("cd amino")
+ elsif num ==4
+  system("cd amino")
   
-   system("bash 1.sh")
+  system("bash 1.sh")
    
-   elsif num ==exit
+ elsif num ==5
+  system("cp -r alhamwi $HOME")
+   
+  print("done")
+   
+  elsif num ==exit
    system("exit && exit")
- else
-   system("ruby SEA-fish.rb")
+     else
+       system("ruby SEA-fish.rb")
  end
  puts "good by"
